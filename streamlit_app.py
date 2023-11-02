36df5f4e33c250e687280693bb6479e3cbe55842
Marco Antonio <85306456+Marconato78@users.noreply.github.com>
2023-11-02 13:24:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.write('Hello world!')
+st.write('Blas de Lezo y Olavarrieta​ fue un almirante español —conocido por la singular estampa que le dieron sus numerosas heridas de guerra')
